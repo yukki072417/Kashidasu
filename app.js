cad7cb045f1f5928c09e9aeab6fb3a173659602b
yukki072417 <yukki072417@gmail.com>
2025-04-03 16:41:23 +0900

    リファクタリング

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: { 
         secure: false,
-        maxAge: 1000 * 60 * 60 * 24 // 24hours
+        maxAge: 1000 * 60 * 60 * 24
     }
 }));
 
