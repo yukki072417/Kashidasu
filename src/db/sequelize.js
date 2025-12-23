f664140a8fd00681fecc3436a4b83c88727d9801
“yukki072417“ <yukki072417@gmail.com>
2025-12-23 19:30:58 +0900

    ホスト名を環境変数から参照

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'db',
+    host: process.env.DB_HOST,
     port: 3306,
     dialect: "mysql"
 });
