8306685c84485ae6c12686f2ad5796590c5cf2a6
“yukki072417“ <yukki072417@gmail.com>
2025-12-23 19:33:38 +0900

    管理者モデル層のCRUD操作を実装

--- src/model/adminModel.js
+++ src/model/adminModel.js
@@ -1,36 +1,61 @@
 const sequelize = require('../db/sequelize');
-const { Admin } = require('../db/init');
+const Admin = require('../db/models/admin');
 
 async function createAdmin(adminId, password) {
-  // サンプル処理
-  if (adminId == null || password == null) throw new Error('adminIdまたはpasswordがnullです');
-
-  try {
-    let admin;
-    sequelize.transaction(async (t) => {
-      admin = await Admin.create({
-        admin_id: adminId,
-        password: password
-      });
-    });
-
-    return admin;
-  } catch (error) {
-    console.error(`Error: adminModel.js: ${error}`)
-    throw error;
+  if (adminId == null || password == null) {
+    throw new Error('Cannot empty adminId and password.');
   }
+
+  const newAdmin = await sequelize.transaction(async (t) => {
+    return Admin.create({
+      admin_id: adminId,
+      password: password
+    }, { transaction: t });
+  });
+
+  return newAdmin;
 }
 
-async function getAdmin() {
+async function getAdmin(adminId) {
+  const admin = await Admin.findOne({
+    where: {
+      admin_id: adminId
+    }
+  });
+  
+  if (!admin) {
+    throw new Error('Admin not found.');
+  }
 
+  const result = {
+    admin_id: admin.admin_id,
+    password: admin.password
+  }
+  
+  return result;
 }
 
-async function updateAdmin() {
+async function updateAdmin(adminId, password) {
+  const [affectedRows] = await sequelize.transaction(async (t) => {
+    return Admin.update({
+      password: password
+    }, {
+      where: {
+        admin_id: adminId
+      },
+      transaction: t
+    });
+  });
 
+  return affectedRows;
 }
 
-async function deleteAdmin() {
-
+async function deleteAdmin(adminId) {
+  return Admin.destroy({
+    where: {
+      admin_id: adminId
+    }
+  });
 }
 
 module.exports = {
