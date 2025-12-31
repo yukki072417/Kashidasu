17ba375ca8bf560ebdc4d1cc58b90f1f452f0d45
“yukki072417“ <yukki072417@gmail.com>
2026-01-01 00:59:33 +0900

    本のモデル層を実装

--- src/model/bookModel.js
+++ src/model/bookModel.js
@@ -2,16 +2,22 @@ const sequelize = require('../db/sequelize');
 const { Book } = require('../db/init');
 const { Op } = require('sequelize');
 
-async function createBook(isbn, name, author, publisher) {
-  const newBook = await sequelize.transaction(async (t) => {
-    return await Book.create({
-      isbn: isbn,
-      name: name,
-      author: author,
-      publisher: publisher
-    }, { transaction: t });
-  });
-  return newBook;
+async function createBook(isbn, title, author, publisher) {
+  try {
+    const newBook = await sequelize.transaction(async (t) => {
+      return Book.create({
+        isbn: isbn,
+        title: title,
+        author: author,
+        publisher: publisher
+      }, { transaction: t });
+    });
+    const obj = newBook.toJSON();
+    return obj;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 }
 
 async function getBookByIsbn(isbn) {
@@ -23,21 +29,27 @@ async function getBookByIsbn(isbn) {
   if (!book) {
     throw new Error('Book not found.');
   }
+  const obj = book.toJSON();
+  return obj;
 }
 
 async function getBookByName(name) {
   const [book] = await Book.findAll({
     where: {
-      [Op.like]: [`%${name}%`]
+      name: { 
+        [Op.like]: `%${name}%` 
+      }
     }
   });
   if(!book){
     throw new Error('Book not found.');
   }
+  const obj = book.toJSON();
+  return obj;
 }
 
 async function updateBook(isbn, changedIsbn, changedName, changedAuthor, changedPublisher) {
-  const [affectedRows] = await sequelize.transaction((t) => {
+  const [affectedRows] = await sequelize.transaction(async (t) => {
     return Book.update({
       isbn: changedIsbn,
       name: changedName,
@@ -55,7 +67,7 @@ async function updateBook(isbn, changedIsbn, changedName, changedAuthor, changed
 }
 
 async function deleteBook(isbn) {
-  const deletedRows = await sequelize.transaction((t) => {
+  const deletedRows = await sequelize.transaction(async (t) => {
     return Book.destroy({
       where: {
         isbn: isbn
@@ -63,6 +75,7 @@ async function deleteBook(isbn) {
       transaction: t
     });
   });
+  return deletedRows;
 }
 
 module.exports = {
