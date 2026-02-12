36c36c21fccb9997af7374c48164ae54465a66d2
yukki072417 <yukki072417@gmail.com>
2026-02-13 06:33:36 +0900

    Adminのコントローラー層を実装中

--- src/controller/adminController.js
+++ src/controller/adminController.js
@@ -1,4 +1,19 @@
-function createAdmin(req, res, next) {}
+const adminModel = require("../model/adminModel");
+const { errorResponse } = require("../services/errorHandling");
+
+async function createAdmin(req, res, next) {
+  const adminData = req.body;
+  if (adminData.id == undefined || adminData.password == undefined) {
+    return errorResponse(res, 200);
+  }
+
+  const result = await adminModel.createAdmin(adminData.id, adminData.password);
+  if (result.success == false) {
+    return errorResponse(res, 500);
+  }
+
+  return res.status(200);
+}
 
 function getAdmin(req, res, next) {}
 
--- src/router/adminRoutes.js
+++ src/router/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const adminController = require("../controller/adminController");
 
-router.post("/register", adminController.createAdmin);
+router.post("/signin", adminController.createAdmin);
 router.get("/get", adminController.getAdmin);
 router.put("/update", adminController.updateAdmin);
 router.delete("/delete", adminController.deleteAdmin);
