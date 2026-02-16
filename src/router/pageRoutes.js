c3beb2f40cdd91e706d971165f9a57543fd09f57
yukki072417 <yukki072417@gmail.com>
2026-02-16 13:19:47 +0900

    ページのレンダリング

--- src/router/pageRoutes.js
+++ src/router/pageRoutes.js
@@ -1,6 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../services/auth");
 
 router.get("/login", (req, res) => res.render("Login"));
+router.get("/edit", (req, res) => {
+  res.render("EditBook");
+});
+router.get("/register", auth.adminAuth, (req, res) => {
+  res.render("Register");
+});
+router.get("/main", auth.adminAuth, auth.renderMainPage);
+router.get("/read-code", (req, res) => {
+  res.render("ReadCode");
+});
+router.get("/generate-card", auth.adminAuth, (req, res) => {
+  res.render("GenerateCard");
+});
+router.get("/book-list", (req, res) => {
+  res.render("BookList");
+});
+router.get("/scanning-registration", auth.adminAuth, (req, res) => {
+  res.render("Registers/ScanningRegister");
+});
+router.get("/collective-registration", auth.adminAuth, (req, res) => {
+  res.render("Registers/CollectiveRegister");
+});
+router.get("/settings", (req, res) => res.render("Settings"));
 
 module.exports = router;
