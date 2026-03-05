3e0641dda283c2f5443617bbc89b5ee9506399b2
yukki072417 <yukki072417@gmail.com>
2026-03-06 08:15:48 +0900

    スペルミスを修正

--- src/db/models/loan.js
+++ src/db/models/loan.js
@@ -5,42 +5,42 @@ const repositoryPath = path.join(__dirname, "../../../repository");
 
 class LoanModel {
   async create(loanId, bookId, userId, loanDate, returnDate = null) {
-    const loans = await readJsonFile(repositoryPath, "loans.json");
-    if (loans.find(loan => loan.loanId === loanId)) {
+    const loans = await readJsonFile(repositoryPath, "loan.json");
+    if (loans.find((loan) => loan.loanId === loanId)) {
       throw new Error("Loan with this ID already exists.");
     }
     loans.push({ loanId, bookId, userId, loanDate, returnDate });
-    await writeJsonFile(repositoryPath, "loans.json", loans);
+    await writeJsonFile(repositoryPath, "loan.json", loans);
   }
 
   async findOne(loanId) {
-    const loans = await readJsonFile(repositoryPath, "loans.json");
+    const loans = await readJsonFile(repositoryPath, "loan.json");
     return loans.find((loan) => loan.loanId === loanId);
   }
 
   async findByBookId(bookId) {
-    const loans = await readJsonFile(repositoryPath, "loans.json");
+    const loans = await readJsonFile(repositoryPath, "loan.json");
     return loans.filter((loan) => loan.bookId === bookId);
   }
 
   async findByUserId(userId) {
-    const loans = await readJsonFile(repositoryPath, "loans.json");
+    const loans = await readJsonFile(repositoryPath, "loan.json");
     return loans.filter((loan) => loan.userId === userId);
   }
 
   async findAll() {
-    const loans = await readJsonFile(repositoryPath, "loans.json");
+    const loans = await readJsonFile(repositoryPath, "loan.json");
     return loans;
   }
 
   async update(loanId, newData) {
-    const loans = await readJsonFile(repositoryPath, "loans.json");
-    const index = loans.findIndex(loan => loan.loanId === loanId);
+    const loans = await readJsonFile(repositoryPath, "loan.json");
+    const index = loans.findIndex((loan) => loan.loanId === loanId);
     if (index === -1) {
       throw new Error("Loan not found.");
     }
     loans[index] = { ...loans[index], ...newData };
-    await writeJsonFile(repositoryPath, "loans.json", loans);
+    await writeJsonFile(repositoryPath, "loan.json", loans);
   }
 }
 
