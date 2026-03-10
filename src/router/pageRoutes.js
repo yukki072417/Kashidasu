77e82b3d98afd7922e2e83d6e792af21a548a81d
yukki072417 <yukki072417@gmail.com>
2026-03-10 13:14:17 +0900

    ファイル名の統一

--- src/router/pageRoutes.js
+++ src/router/pageRoutes.js
@@ -10,21 +10,21 @@ const apiKey = process.env.BOOKS_API_KEY;
 /**
  * ログインページ
  */
-router.get("/", (req, res) => res.render("login"));
-router.get("/login", (req, res) => res.render("login"));
+router.get("/", (req, res) => res.render("Login"));
+router.get("/login", (req, res) => res.render("Login"));
 
 /**
  * 書籍編集ページ
  */
 router.get("/edit", (req, res) => {
-  res.render("editbook");
+  res.render("EditBook");
 });
 
 /**
  * 書籍登録ページ（認証必須）
  */
 router.get("/register", auth.adminAuth, (req, res) => {
-  res.render("register");
+  res.render("Register");
 });
 
 /**
@@ -36,41 +36,41 @@ router.get("/main", auth.adminAuth, auth.renderMainPage);
  * バーコード読み取りページ
  */
 router.get("/read-code", (req, res) => {
-  res.render("readcode");
+  res.render("ReadCode");
 });
 
 /**
  * カード生成ページ（認証必須）
  */
 router.get("/generate-card", auth.adminAuth, (req, res) => {
-  res.render("generatecard");
+  res.render("GenerateCard");
 });
 
 /**
  * 書籍一覧ページ
  */
 router.get("/book-list", (req, res) => {
-  res.render("booklist");
+  res.render("BookList");
 });
 
 /**
  * スキャン登録ページ（認証必須）
  */
 router.get("/scanning-registration", auth.adminAuth, (req, res) => {
-  res.render("Registers/scanningregister", { apiKey: apiKey });
+  res.render("Registers/ScanningRegister", { apiKey: apiKey });
 });
 
 /**
  * 一括登録ページ（認証必須）
  */
 router.get("/collective-registration", auth.adminAuth, (req, res) => {
-  res.render("Registers/collectiveregister");
+  res.render("Registers/CollectiveRegister");
 });
 
 /**
  * 設定ページ
  */
-router.get("/settings", (req, res) => res.render("settings"));
+router.get("/settings", (req, res) => res.render("Settings"));
 
 /**
  * ログアウト処理
